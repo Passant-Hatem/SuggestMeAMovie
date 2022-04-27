ce0528f07e5fa52cda60f45bc56a9bca3519c1d7
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2022-04-27 04:25:16 +0200

    basic design

---
+++ app/src/main/java/com/example/suggestmeamovie/Popular.java
@@ -0,0 +1,19 @@
+package com.example.suggestmeamovie;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Popular extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_popular, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/suggestmeamovie/TopRated.java
@@ -0,0 +1,19 @@
+package com.example.suggestmeamovie;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopRated extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top_rated, container, false);
+    }
+}
